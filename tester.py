e416bb24c4a751bde5d0344b34b2386b3b6fe4ae
Po Chen <pchen@zendesk.com>
2014-07-14 23:07:07 +1000

    Improve script

--- tester.py
+++ tester.py
@@ -2,6 +2,9 @@ import requests
 import sys
 
 def main():
-  requests.post("http://localhost:7654/webhooks/hit/{}".format(sys.argv[1]), { "ref": "abcdefg", "head_commit": { "message": "Merge" } })
+  requests.post(
+    "http://localhost:7654/webhooks/hit/{}".format(len(sys.argv) > 1 and sys.argv[1] or "abc123"),
+    { "ref": "abcdefg", "head_commit": { "message": "Merge" } }
+  )
 
 main()
