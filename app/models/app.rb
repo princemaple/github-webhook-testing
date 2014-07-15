b605fb0c6c73e00fa8dc65d71fe9c027b735f110
Po Chen <chenpaul914@gmail.com>
2014-07-15 20:47:06 +1000

    Create app.rb

---
+++ app/models/app.rb
@@ -0,0 +1,17 @@
+class App < ActiveRecord::Base
+
+  ## ### Response
+  ##
+  ## ```js
+  ## {
+  ##  "id": 1,
+  ##  "name": "Tester",
+  ##  "single_install": true
+  ## }
+  ## ```
+  def as_json
+    # this comment will not be included in the documentation
+    super(:only => [:id, :name, :single_install])
+  end
+
+end
