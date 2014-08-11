49f3233ea04d48b7d70167247d74424710f420e4
Po Chen <pchen@zendesk.com>
2014-08-11 14:47:32 +1000

    Use official ref

--- tester.py
+++ tester.py
@@ -4,7 +4,7 @@ import sys
 def main():
   requests.post(
     "https://hercules.zendesk.dev/webhooks/github/{}".format(len(sys.argv) > 1 and sys.argv[1] or "abc123"),
-    { "ref": "abcdefg", "head_commit[id]": "123456789abcdefg", "head_commit[message]": "Merge" },
+    { "ref": "refs/heads/master", "head_commit[id]": "123456789abcdefg", "head_commit[message]": "Merge" },
     verify=False
   )
 
